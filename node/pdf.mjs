90d5c051fa8b0aa77b045397e1b8d56290116cc9
dbelenguer <david.belenguer.ruiz@gmail.com>
2022-05-16 13:06:00 +0200

    Changing pdf generation

--- node/pdf.mjs
+++
@@ -1,8 +0,0 @@
-import { createReadStream, createWriteStream } from 'fs';
-import markdownpdf from 'markdown-pdf';
-
-const { pathname: index } = new URL('../index.md', import.meta.url)
-
-createReadStream(index)
-    .pipe(markdownpdf())
-    .pipe(createWriteStream('dbelenguer-cv.pdf'))
